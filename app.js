d2baf6435cee630d819c2af5d091e5995a22a8fb
Nathan Schneider <n.l.schneider@gmail.com>
2018-08-11 23:12:15 -0400

    removed private stuff

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const request = require('request'),
         url: 'https://www.cradlepointecm.com/api/v2/routers/?limit=500',
         method: 'GET',
         headers: {
-            'X-CP-API-ID': 'e79c6722',
-            'X-CP-API-KEY': '11248c51069c2e200d1e89c74830c431',
-            'X-ECM-API-ID': 'a86d9b3c-6f1b-498d-907a-bd2facf56bc9',
-            'X-ECM-API-KEY': '5d707dd2d354f0cc125cb43e7b365b972f7454c2',
+            'X-CP-API-ID': '...',
+            'X-CP-API-KEY': '...',
+            'X-ECM-API-ID': '...',
+            'X-ECM-API-KEY': '...',
             'Content-Type': 'application/json'
         }
     };
